db6793de1123df4cde00ddfbc3267cd45dd01dab
Hui Xiong <hui.xiong@cdcr.ca.gov>
2026-06-03 10:39:28 -0700

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -28,8 +28,13 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
+    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME']== fruit_chosen, 'search_on'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', serach_on, '.')
+
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
