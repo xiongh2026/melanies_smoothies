483b4f31bf92e0f012c61bc609a7de8469209661
Hui Xiong <hui.xiong@cdcr.ca.gov>
2026-06-03 11:33:29 -0700

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 # Write directly to the app
 st.title('Customize Your Smoothie! :cup_with_straw:')
