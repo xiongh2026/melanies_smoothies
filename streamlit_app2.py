a8f6cf25f8c90f4aa65b7ac3a4c0a768d32d8624
Hui Xiong <hui.xiong@cdcr.ca.gov>
2026-06-03 09:09:19 -0700

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -31,3 +31,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+
+#New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
